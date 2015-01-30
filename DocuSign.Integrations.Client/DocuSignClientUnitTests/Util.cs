1e45fa371e3ca908d88a40a91785ce0c51dc14a6
Joe Cartano <joe.cartano@docusign.com>
2015-01-30 14:54:59 -0800

    remove unused using statements (test commit/push)

--- DocuSign.Integrations.Client/DocuSignClientUnitTests/Util.cs
+++ DocuSign.Integrations.Client/DocuSignClientUnitTests/Util.cs
@@ -7,9 +7,6 @@
 namespace RestClientUnitTests
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
 
     /// <summary>
     /// TODO: Update summary.

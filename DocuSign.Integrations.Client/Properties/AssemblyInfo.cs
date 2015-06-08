10bb404afcd075e6bd20f04168c34089e13e34c3
Inbar Gazit <inbar.gazit@docusign.com>
2015-06-08 10:36:15 -0700

    updating AssemblyInfo.cs in prep for getting the 1.4 Nuget out the door...

--- DocuSign.Integrations.Client/Properties/AssemblyInfo.cs
+++ DocuSign.Integrations.Client/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DocuSign.Integration.Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("DocuSign")]
 [assembly: AssemblyProduct("DocuSign.Integration.Client")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyCopyright("Copyright © DocuSign 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]

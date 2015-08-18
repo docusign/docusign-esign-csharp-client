e376435990b8011e342fc9e68b4a213a6da547d0
Ergin Dervisoglu <ergin.dervisoglu@docusign.com>
2015-08-18 12:48:40 -0700

    default REST client settings to demo environment

--- DocuSign.Integrations.Client/RestSettings.cs
+++ DocuSign.Integrations.Client/RestSettings.cs
@@ -53,8 +53,6 @@ namespace DocuSign.Integrations.Client
                         // default to Demo environment and latest API version (v2)
                         instance.DocuSignAddress = "https://demo.docusign.net";
                         instance.WebServiceUrl = RestSettings.Instance.DocuSignAddress + "/restapi/v2";
-
-                        Console.WriteLine("DEFAULT SINGLETON DATA SET!");
                     }
 
                     slim.ExitWriteLock();

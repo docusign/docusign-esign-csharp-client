e0e05429155a629966e793140efbe472485599a0
Inbar Gazit <inbar.gazit@docusign.com>
2015-11-23 09:02:22 -0800

    going back to .NET 4.0 so we can support older VS clients as well as customers using various .NET 4.0 packages that do not fully support .NET 4.5 yet

--- DocuSign.Integrations.Client/Template.cs
+++ DocuSign.Integrations.Client/Template.cs
@@ -6,11 +6,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 
 namespace DocuSign.Integrations.Client

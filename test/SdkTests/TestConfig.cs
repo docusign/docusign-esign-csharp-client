aa9ba346bf185bae166ef2e3862e35940d4d904f
Naveen Gopala <naveen.gopala@docusign.com>
2017-11-07 00:51:03 -0800

    Updated the test config for jwt tests

--- test/SdkTests/TestConfig.cs
+++ test/SdkTests/TestConfig.cs
@@ -37,9 +37,9 @@ namespace SdkTests
 
             this.ReturnUrl = (returnUrl != null) ? returnUrl : "https://www.docusign.com/devcenter";
 
-            this.UserId = "REQUIRED";
+            this.UserId = "***REMOVED***";
             this.OAuthBasePath = "account-d.docusign.com";
-            this.PrivateKeyFilename = "REQUIRED";
+            this.PrivateKeyFilename = "../../docs/private.pem";
             this.ExpiresInHours = 1;
 
             //this.Configuration = new Configuration();

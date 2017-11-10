bd47f7dcd272de58593be04b7ab017bbb9b731d9
Naveen Gopala <gsnavin@gmail.com>
2017-11-09 16:41:02 -0800

    Trying out a solution to fix the travis build

---
+++ test/SdkTests/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace Microsoft.VisualStudio.TestTools.UnitTesting
+{
+    public class Placeholder { }
+    public class TestClassAttribute : NUnit.Framework.TestFixtureAttribute
+    {
+    }
+    public class TestInitializeAttribute : NUnit.Framework.SetUpAttribute
+    {
+    }
+    public class TestMethodAttribute : NUnit.Framework.TestAttribute
+    {
+    }
+    public class TestCleanupAttribute : NUnit.Framework.TearDownAttribute
+    {
+    }
+    public class IgnoreAttribute : NUnit.Framework.IgnoreAttribute
+    {
+    }
+    public class ExpectedExceptionAttribute : NUnit.Framework.ExpectedExceptionAttribute
+    {
+        public ExpectedExceptionAttribute(Type exceptionType) : this(exceptionType, null)
+        {
+        }
+        public ExpectedExceptionAttribute(Type exceptionType, string message) : base(exceptionType)
+        {
+            UserMessage = message;
+        }
+    }
+    public class TestContext : NUnit.Framework.TestContext
+    {
+        public TestContext(IDictionary dictionary) : base(dictionary)
+        {
+        }
+    }
+    public class Assert : NUnit.Framework.Assert
+    {
+        public static void IsInstanceOfType(object obj, Type type)
+        {
+            NUnit.Framework.Assert.IsInstanceOfType(type, obj, null);
+        }
+        public static void IsInstanceOfType(object obj, Type type, string message)
+        {
+            NUnit.Framework.Assert.IsInstanceOfType(type, obj, message);
+        }
+    }
+}

1efad508059234781f844a5a0d3a133bfa974fd6
RajRele <rele.bharat@gmail.com>
2018-10-15 13:24:30 -0700

    v3.1.1 SDK release

--- sdk/src/DocuSign.eSign/Client/Configuration.cs
+++ sdk/src/DocuSign.eSign/Client/Configuration.cs
@@ -46,7 +46,7 @@ namespace DocuSign.eSign.Client
                              string tempFolderPath = null,
                              string dateTimeFormat = null,
                              int timeout = 100000,
-                             string userAgent = "Swagger-Codegen/3.1.0-rc/csharp"
+                             string userAgent = "Swagger-Codegen/3.1.1/csharp"
                             )
         {
             setApiClientUsingDefault(apiClient);
@@ -81,7 +81,7 @@ namespace DocuSign.eSign.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "3.1.0-rc";
+        public const string Version = "3.1.1";
 
         /// <summary>
         /// Gets or sets the default Configuration.
@@ -322,7 +322,7 @@ namespace DocuSign.eSign.Client
                      .GetReferencedAssemblies()
                      .Where(x => x.Name == "System.Core").First().Version.ToString()  + "\n";
             report += "    Version of the API: v2\n";
-            report += "    SDK Package Version: 3.1.0-rc\n";
+            report += "    SDK Package Version: 3.1.1\n";
 
             return report;
         }

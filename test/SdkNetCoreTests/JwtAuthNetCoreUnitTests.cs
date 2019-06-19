1e84f24a26640387c58bbfc8c61b0121091056c5
RajRele <rele.bharat@gmail.com>
2019-06-19 12:30:34 -0700

    changin the private keys

--- test/SdkNetCoreTests/JwtAuthNetCoreUnitTests.cs
+++ test/SdkNetCoreTests/JwtAuthNetCoreUnitTests.cs
@@ -21,8 +21,10 @@ namespace SdkNetCoreTests
         {
             testConfig.ApiClient = new ApiClient(testConfig.Host);
 
-            // Create a stream of bytes... 
-            byte[] privateKeyStream = File.ReadAllBytes(testConfig.PrivateKeyFilename);
+            Assert.IsNotNull(testConfig.PrivateKey);
+
+            byte[] privateKeyStream = Convert.FromBase64String(testConfig.PrivateKey);
+
             OAuth.OAuthToken tokenInfo = testConfig.ApiClient.RequestJWTUserToken(testConfig.IntegratorKey, testConfig.UserId, testConfig.OAuthBasePath, privateKeyStream, testConfig.ExpiresInHours);
             // Disposing the stream...
 
@@ -512,7 +514,7 @@ namespace SdkNetCoreTests
         public void JwtInvalidGrantTest()
         {
             // Adding a WRONG PEM key 
-            byte[] privateKeyStream = File.ReadAllBytes(testConfig.PrivateKeyFilename);
+            byte[] privateKeyStream = Convert.FromBase64String(testConfig.PrivateKey);
 
             ApiException ex = Assert.ThrowsException<ApiException>(() => testConfig.ApiClient.RequestJWTUserToken(testConfig.IntegratorKeyNoConsent, testConfig.UserId, testConfig.OAuthBasePath, privateKeyStream, testConfig.ExpiresInHours));
 
@@ -524,7 +526,7 @@ namespace SdkNetCoreTests
         public void JwtConsentRequiredTest()
         {
             // Adding a Correct PEM key - no consent granted
-            byte[] pkey = System.Text.Encoding.UTF8.GetBytes(File.ReadAllText(testConfig.PrivateKeyNoConsentFilename));
+            byte[] pkey = System.Text.Encoding.UTF8.GetBytes(File.ReadAllText(testConfig.PrivateKeyNoConsent));
             ApiException ex = Assert.ThrowsException<ApiException>(() => testConfig.ApiClient.RequestJWTUserToken(testConfig.IntegratorKeyNoConsent, testConfig.UserId, testConfig.OAuthBasePath, pkey, testConfig.ExpiresInHours));
 
             Assert.IsNotNull(ex);

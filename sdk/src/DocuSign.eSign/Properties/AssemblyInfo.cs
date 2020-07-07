313d7ca0ce8626945eb231c9c25ec49abb64da6b
RajRele <rele.bharat@gmail.com>
2020-07-06 17:24:32 -0700

    v3.5.2-rc updates

--- sdk/src/DocuSign.eSign/Properties/AssemblyInfo.cs
+++ sdk/src/DocuSign.eSign/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 internal class AssemblyInformation
 {
-    public const string AssemblyInformationalVersion = "3.5.1-rc";
+    public const string AssemblyInformationalVersion = "3.5.2-rc";
 }

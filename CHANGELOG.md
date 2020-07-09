0fa71517d4d747d153884a1b84a93fdbd8ec5b20
RajRele <rele.bharat@gmail.com>
2020-07-09 15:43:03 -0700

    v3.5.2GA Commit

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 ﻿# DocuSign C# Client Changelog - eSignature API v2
 
+## [v3.5.2] - eSignature API v2-20.2.00 - 07/09/2020
+### Changed
+*   Added support for version v2-20.2.00 of the DocuSign eSignature API.
+*   Updated the SDK release version.
+
 ## [v3.4.1] - eSignature API v2-20.1.02 - 05/29/2020
 ### Changed
 *   Added support for version v2-20.1.02 of the DocuSign eSignature API.
--- sdk/src/DocuSign.eSign/Client/Configuration.cs
+++ sdk/src/DocuSign.eSign/Client/Configuration.cs
@@ -47,7 +47,7 @@ namespace DocuSign.eSign.Client
                              string tempFolderPath = null,
                              string dateTimeFormat = null,
                              int timeout = 100000,
-                             string userAgent = "Swagger-Codegen/3.5.2-rc/csharp"
+                             string userAgent = "Swagger-Codegen/3.5.2/csharp"
                             )
         {
             setApiClientUsingDefault(apiClient);
@@ -82,7 +82,7 @@ namespace DocuSign.eSign.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "3.5.2-rc";
+        public const string Version = "3.5.2";
 
         /// <summary>
         /// Gets or sets the default Configuration.
@@ -344,7 +344,7 @@ namespace DocuSign.eSign.Client
                      .GetReferencedAssemblies()
                      .Where(x => x.Name == "System.Core").First().Version.ToString() + "\n";
             report += "    Version of the API: v2\n";
-            report += "    SDK Package Version: 3.5.2-rc\n";
+            report += "    SDK Package Version: 3.5.2\n";
 
             return report;
         }

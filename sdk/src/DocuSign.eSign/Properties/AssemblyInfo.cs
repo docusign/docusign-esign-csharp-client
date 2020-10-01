2d097dd6346ec36fd246d993a0179445e0825122
Bharat Rele <rele.bharat@gmail.com>
2020-10-01 14:51:06 -0700

    v2-3.7.0GA (#299)

--- sdk/src/DocuSign.eSign/Properties/AssemblyInfo.cs
+++ sdk/src/DocuSign.eSign/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 internal class AssemblyInformation
 {
-    public const string AssemblyInformationalVersion = "3.7.0-rc";
+    public const string AssemblyInformationalVersion = "3.7.0";
 }
